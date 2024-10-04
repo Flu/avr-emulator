746083677d583c7b1ccba56f9d120225e0b86ce7
Adrian Fluturel <fluturel.adrian@gmail.com>
2024-10-04 22:57:36 +0200

    Change test for call

--- tests/test_call.asm
+++ tests/test_call.asm
@@ -1,15 +1,51 @@
-    LDI R0, 0x0
-    LDI R1, 0x0
-    CALL function
+;; (Copyright 2024), Adrian Fluturel
+;; Program that tries every combination of all 8-bit values, tries to multiply them using a custom multiplication function
+;; and compares the result with the MUL built-in instruction. If all results match, R31 will be set to 0x1. If one of them
+;; is different, R31 is 0x00. 
+    
+    JMP _main
 
-    LDI R17, 0x1
-    JMP end
+;; Multiplication function
+;; First argument in R17, second argument in R18
+;; Result in R16
+mul_function:
+    PUSH R17 ; Save R17, R18 to the stack
+    PUSH R18
+    LDI R16, 0x0 ; Clear R16 of old values
+    loop:
+    SBRC R18, 0
+    ADD R16, R17
+    LSL R17
+    LSR R18
+    BRNE loop
 
-function:
-    LDI R20, 0x1
-    LDI R21, 0x2
-    ADD R20, R21
+    POP R18 ; Restore R18 and R17
+    POP R17
     RET
+;; End of multiplication function
 
+_main:
+    ; Go through every combination of all 8-bit values for R17 and R18 and test them
+    LDI R17, 0xff
+    r17_loop:
+
+    LDI R18, 0xff
+    r18_loop:
+
+    ; Test whether the answer of our multiply function is equal to the low byte of the MUL instruction
+    CALL mul_function
+    MUL R17, R18
+    ; The answer for MUL is in the R0 (low byte in R0, high in R1)
+    CP R16, R0
+    ; Jump to the end if the answers are different, continue otherwise
+    BRNE end
+
+    SUBI R18, 0x1
+    BRNE r18_loop
+
+    SUBI R17, 0x1
+    BRNE r17_loop
+
+    ; Set R31 to 1 if all the answers are correct, else 0
+    LDI R31, 0x01
 end:
-    NOP
