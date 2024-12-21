c3275fe37a3b03c756d015d01e263cb5550d6ea5
Adrian Fluturel <fluturel.adrian@gmail.com>
2024-12-22 00:44:48 +0100

    + Add help screen - Remove bug where encountering a jump instruction crashes the program

--- src/Repl.hs
+++ src/Repl.hs
@@ -12,11 +12,12 @@ import System.Console.Haskeline
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-import Emulator (Instruction, EmulatorState (..), initEmulatorState, printRegisterBank, registers, showStatusFlags, flags, stepOneInstruction, stepMultipleInstructions)
+import Emulator (Instruction, EmulatorState (..), initEmulatorState, printRegisterBank, registers, showStatusFlags, flags, stepOneInstruction, stepMultipleInstructions, replaceLabels)
 import Emulator.State (EmulatorState(EmulatorState))
 import Emulator.Utils (toHex4)
 import Data.Text.Internal.Builder.Int.Digits (digits)
 import Numeric (readInt, readDec)
+import Data.Maybe (catMaybes)
 
 type Parser = Parsec Void T.Text
 
@@ -27,6 +28,7 @@ data UserCommand
     | PrintFlags
     | PrintCurrentInstruction
     | PrintPc
+    | Help
     | Quit
 
 digitParser :: Parser Char
@@ -66,6 +68,11 @@ printProgramCounterCommandParser = do
     string "pc" <|> string "p"
     return PrintPc
 
+helpCommandParser :: Parser UserCommand
+helpCommandParser = do
+    string "help"
+    return Help
+
 quitCommandParser :: Parser UserCommand
 quitCommandParser = do
     string "quit" <|> string "q"
@@ -80,6 +87,7 @@ commandParser =
         try printFlagsCommandParser,
         try printProgramCounterCommandParser,
         try printCurrentInstructionCommandParser,
+        try helpCommandParser,
         try quitCommandParser]
 
 parseCommand :: String -> Either (ParseErrorBundle T.Text Void) UserCommand
@@ -88,12 +96,12 @@ parseCommand command = runParser commandParser "" $ T.pack command
 dispatcher :: UserCommand -> Array Int Instruction -> EmulatorState -> InputT IO (EmulatorState)
 dispatcher StepOnce programMemory state = do
     let updatedState = stepOneInstruction programMemory state
-    outputStrLn $ "PC: " ++ (toHex4 $ fromIntegral $ programCounter updatedState)
+    outputStrLn $ "PC: 0x" ++ (toHex4 $ fromIntegral $ programCounter updatedState)
     return updatedState
 
 dispatcher (StepMultiple n) programMemory state = do
     let updatedState = stepMultipleInstructions programMemory state n
-    outputStrLn $ "PC: " ++ (toHex4 $ fromIntegral $ programCounter updatedState)
+    outputStrLn $ "PC: 0x" ++ (toHex4 $ fromIntegral $ programCounter updatedState)
     outputStrLn $ show n
     return updatedState
 
@@ -114,6 +122,12 @@ dispatcher PrintCurrentInstruction instructions state = do
     outputStrLn $ show $ instructions ! (fromIntegral $ programCounter state)
     return state
 
+dispatcher Help _ state = do
+    outputStrLn helpText
+    return state
+
+-- TODO: this currently has no effect on the REPL
+-- Modify it to actually quit the REPL
 dispatcher Quit instructions state = do
     outputStrLn "Exited."
     return state
@@ -123,7 +137,8 @@ replLoop :: [Instruction] -> Int -> IO (EmulatorState)
 replLoop instructions memorySize = runInputT defaultSettings (loop initialState)
   where
     initialState = initEmulatorState memorySize
-    programMemory = listArray (0, (length instructions) - 1) instructions
+    resolvedInstructions = catMaybes $ replaceLabels instructions
+    programMemory = listArray (0, (length resolvedInstructions) - 1) resolvedInstructions
     loop :: EmulatorState -> InputT IO (EmulatorState)
     loop state = do
       rawInput <- getInputLine "avr-emulator> "
@@ -140,3 +155,16 @@ replLoop instructions memorySize = runInputT defaultSettings (loop initialState)
         Nothing -> outputStrLn "Exited."
 
       return (state)
+
+helpText :: String
+helpText = unlines
+  ["Available commands:"
+  , "  step | s           Execute the next instruction in the program"
+  , "  step <n> | s <n>   Execute the next n instructions"
+  , "  registers | r      Print the current values of the register"
+  , "  flags | f          Print the current values of the status flags"
+  , "  pc | p             Print the ccurent value of the program counter"
+  , "  instruction | i    Print the next instruction to be executed"
+  , "  help               Print this message"
+  , "  Control-D          Exit the REPL"
+  ]
